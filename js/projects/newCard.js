cf1f5dab48615fa7aa846d043dea12d1e7ed1bfc
oliviaaMach <olivia.mach@outlook.com>
2026-01-06 23:13:29 +0100

    getting the img to work

--- js/projects/newCard.js
+++ js/projects/newCard.js
@@ -4,7 +4,7 @@ import { projectCard } from "./projectCard.js";
 export const myProjects = [
     new projectCard({
         title: "Pokémon API",
-        image: "./assets/imgs/pokedex.PNG",
+        image: "../assets/imgs/pokedex.PNG",
         info: "This project is build with HTML, CSS and JavaScript. The user can use the right and left buttons to go through all the OG Pokémons. ",
         tags: ['HTML','CSS', 'JavaScript'],
         link: "https://oliviaamach.github.io/pokiAPI/",
@@ -13,7 +13,7 @@ export const myProjects = [
 
     new projectCard({
         title: "Ping Pong Game",
-        image: "./assets/imgs/pingPong.PNG",
+        image: "../assets/imgs/pingPong.PNG",
         info: "A simple two-player Pong game built using vanilla JavaScript and the HTML5 Canvas API. This project recreates the classic arcade experience: move the paddles, hit the ball, and score points!",
         tags: ['HTML', 'CSS', 'JavaScript'],
         link: "https://oliviaamach.github.io/pingPong-game/",
@@ -22,7 +22,7 @@ export const myProjects = [
 
     new projectCard({
         title: "To Do List",
-        image: "./assets/imgs/toDo.PNG",
+        image: "../assets/imgs/toDo.PNG",
         info: "A simple and elegant To-Do List web application built with HTML, CSS, and JavaScript. This app allows users to add, mark as completed, and delete tasks — with data persistence using localStorage, so your tasks remain even after refreshing the page.",
         tags: ['HTML', 'CSS', 'JavaScript'],
         link: "https://oliviaamach.github.io/todolist/",
