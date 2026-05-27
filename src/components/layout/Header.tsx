a52c03b12a72325fbc260baa2fa2dc15b7e67c4a
oliviaaMach <olivia.mach@outlook.com>
2026-05-27 21:57:06 +0200

    fixed faviicon and logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,11 @@
 import "./Header.css"
+import logo from "../../../public/images/logo.png"
 
 export default function Header () {
     return(
         <header className="header">
             <nav className="navbar">
-                <a href="#home" className="logo">O.M</a>
+                <a href="#home" className="logo"><img src={logo} /></a>
                 <ul className="nav-links">
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
