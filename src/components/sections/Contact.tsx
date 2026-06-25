f440ead310393d5e3736c69dbfea1bef73dc09c8
oliviaaMach <olivia.mach@outlook.com>
2026-06-25 20:42:14 +0200

    changed text for contact

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -3,19 +3,19 @@ import Section from "../layout/Section";
 
 type ContactProps = {
   variant?: "primary-bg" | "secondary-bg"
-} 
+}
 
-export default function Contact({variant = "secondary-bg"}: ContactProps) {
+export default function Contact({ variant = "secondary-bg" }: ContactProps) {
   return (
-    <Section className="contact" id="contact" variant= {variant}>
+    <Section className="contact" id="contact" variant={variant}>
       <div className="contact-container">
         <p className="section-label">Contact</p>
 
-        <h2>Let’s connect</h2>
+        <h2>Let's connect</h2>
 
         <p className="contact-text">
-          I’m currently looking for a LIA placement and junior opportunities.
-          Feel free to contact me if you think I could be a good fit.
+          I'm always open to opportunities where I can grow through collaboration
+          and contribute to exciting projects.
         </p>
 
         <div className="contact-links">
@@ -23,11 +23,15 @@ export default function Contact({variant = "secondary-bg"}: ContactProps) {
             Email me
           </a>
 
-          <a href="https://www.linkedin.com/in/olivia-mach-0b433811a/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/olivia-mach-0b433811a/"
+            target="_blank"
+            rel="noreferrer"
+          >
             LinkedIn
           </a>
 
-          <a href="https://github.com/oliviaaMach" target="_blank">
+          <a href="https://github.com/oliviaaMach" target="_blank" rel="noreferrer">
             GitHub
           </a>
         </div>
